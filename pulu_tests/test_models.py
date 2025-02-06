f1d69995b99e02ed13a4e7bd0e20b8cc3f5d9ce7
Daniel Prange <daniel.prange@anders.com>
2025-02-06 17:33:44 +0200

    feat!: add default ordering for notification
    type (desc) > valid from (desc) > created at (desc)
    
    Refs: KER-442

--- pulu_tests/test_models.py
+++ pulu_tests/test_models.py
@@ -1,9 +1,10 @@
 from collections.abc import Collection
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Union
 
 import pytest
 from django.db.models import QuerySet
+from django.utils import timezone
 from freezegun import freeze_time
 
 from pulu.models import Notification
@@ -18,11 +19,16 @@ def _values_list(objects: Union[Collection, QuerySet[Notification]], *fields: st
     return [(getattr(obj, field) for field in fields) for obj in objects]
 
 
-def assert_qs_values(qs: QuerySet, values: Collection, *fields: str):
+def assert_qs_values(
+    qs: QuerySet, values: Collection, *fields: str, ordered: bool = False
+):
     """Assert that the values of the given fields match between a query set and
     a collection of objects."""
     assert len(qs) == len(values)
-    assert set(_values_list(qs, *fields)) == set(_values_list(values, *fields))
+    if ordered:
+        assert list(_values_list(qs, *fields)) == _values_list(values, *fields)
+    else:
+        assert set(_values_list(qs, *fields)) == set(_values_list(values, *fields))
 
 
 @freeze_time("2025-01-01T12:00:00Z")
@@ -65,3 +71,70 @@ def test_notification_model_managers(notification_factory):
 
     assert_qs_values(Notification.objects.all(), created_notifications, "id")
     assert_qs_values(Notification.valid_objects.all(), valid_notifications, "id")
+
+
+@freeze_time("2025-01-01T12:00:00Z")
+@pytest.mark.django_db
+def test_notification_default_ordering(valid_notification_factory):
+    """Notifications should order with the following priority:
+    1. Type, error > warning > info (desc)
+    2. Validity period start, most recent first (desc)
+    3. Creation time, newest first (desc)
+    """
+    last_hour = timezone.now() - timedelta(hours=1)
+    last_day = timezone.now() - timedelta(days=1)
+    last_month = timezone.now() - timedelta(days=30)
+    expected_order = [
+        # Error
+        valid_notification_factory(
+            type=Notification.Type.ERROR, validity_period_start=last_hour
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ERROR, validity_period_start=last_hour
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ERROR, validity_period_start=last_day
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ERROR, validity_period_start=last_day
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ERROR, validity_period_start=last_month
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ERROR, validity_period_start=last_month
+        ),
+        # Alert
+        valid_notification_factory(
+            type=Notification.Type.ALERT, validity_period_start=last_hour
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ALERT, validity_period_start=last_day
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ALERT, validity_period_start=last_day
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ALERT, validity_period_start=last_day
+        ),
+        valid_notification_factory(
+            type=Notification.Type.ALERT, validity_period_start=last_month
+        ),
+        # Info
+        valid_notification_factory(
+            type=Notification.Type.INFO, validity_period_start=last_hour
+        ),
+        valid_notification_factory(
+            type=Notification.Type.INFO, validity_period_start=last_day
+        ),
+        valid_notification_factory(
+            type=Notification.Type.INFO, validity_period_start=last_day
+        ),
+        valid_notification_factory(
+            type=Notification.Type.INFO, validity_period_start=last_day
+        ),
+    ]
+
+    assert_qs_values(
+        Notification.valid_objects.all(), expected_order, "id", ordered=True
+    )
