b7aaba86faaaca1ca2f4b939975ac9d6d420ab63
Daniel Prange <daniel.prange@anders.com>
2025-02-10 12:49:29 +0200

    test: refactor tests
    Add RelativeTime fixture, move utils to their own module
    
    Refs: KER-442

---
+++ pulu_tests/utils.py
@@ -0,0 +1,26 @@
+from typing import Collection, Union
+
+from django.db.models import QuerySet
+
+from pulu.models import Notification
+
+
+def values_list(objects: Union[Collection, QuerySet[Notification]], *fields: str):
+    flat = len(fields) == 1
+    if isinstance(objects, QuerySet):
+        return objects.values_list(*fields, flat=flat)
+    if flat:
+        return [getattr(obj, fields[0]) for obj in objects]
+    return [(getattr(obj, field) for field in fields) for obj in objects]
+
+
+def assert_qs_values(
+    qs: QuerySet, values: Collection, *fields: str, ordered: bool = False
+):
+    """Assert that the values of the given fields match between a query set and
+    a collection of objects."""
+    assert len(qs) == len(values)
+    if ordered:
+        assert list(values_list(qs, *fields)) == values_list(values, *fields)
+    else:
+        assert set(values_list(qs, *fields)) == set(values_list(values, *fields))
